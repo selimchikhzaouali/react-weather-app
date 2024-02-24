0271e6c215e006bda2a7385b5154a06425a6212a
Selim CHIKH ZAOUALI <selim.chikhzaouali@gmail.com>
2024-02-25 00:08:15 +0100

    implementing icons and more conditions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import React, { useState } from 'react';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import CloudIcon from '@material-ui/icons/Cloud';
+
+const api = {
+  key: "211740d2e64d994282006cbc6f206831",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+  
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -12,23 +34,55 @@ function App() {
 
     return `${day} ${date} ${month} ${year}`
   }
+
   return (
-    <div className="app">
+    <div className={
+      (typeof weather.main != "undefined") ? 
+          ((weather.weather[0].main === 'Thunderstorm' || weather.weather[0].main === 'Rain') ? 'cold-text app rainy' : 
+          ((weather.main.temp < 16) ? 'cold-text app cold' : 'sunny-text app sunny')) 
+        : 'app'
+      }>
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search ..." />
+          <input 
+            type="text"
+            className="search-bar" 
+            placeholder="Search ..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div> 
 
-        <div className="location-box">
-          <div className="location">Tunis</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            15°c
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
-          <div className="weather">Sunny</div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather-description">
+              {weather.weather[0].description}
+              <span className='weather-icon'>
+                { weather.weather[0].main !== "Rain" 
+                  && weather.weather[0].main !== "Thunderstorm" 
+                  && weather.weather[0].main !== "Clouds" 
+                  ? <WbSunnyIcon /> : <CloudIcon />}
+              </span>  
+            </div>
+          </div>
         </div>
+        ) : (
+          <div className='empty-box'>
+              Enter a city name ..
+              <div className='big-icon'><CloudIcon /></div>
+              <div className='big-icon'><WbSunnyIcon /></div>
+          </div>
+        )}
       </main>
     </div>
   );
